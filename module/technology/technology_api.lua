5ae3216183352ddd4777d1c6bc95080b4e7bc0c1
totalwarANGEL <n3crosh8d3@gmail.com>
2023-08-31 22:48:15 +0200

    Add technology module
    Allows to create pseudo technologies.

---
+++ module/technology/technology_api.lua
@@ -0,0 +1,7 @@
+Lib.Require("comfort/IsLocalScript");
+Lib.Register("module/technology/Technology_API");
+
+function AddCustomTechnology(_Key, _Name, _Icon)
+    return Lib.Technology.Shared:AddCustomTechnology(_Key, _Name, _Icon);
+end
+
